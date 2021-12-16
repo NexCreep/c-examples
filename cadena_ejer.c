7b937e4434f823b4b16e7777aa6353202cd6e417
raulop194 <raulop194@alu.centroafuera.es>
2021-12-16 14:12:56 +0100

    Update 16/12 14:12

--- cadena_ejer.c
+++ cadena_ejer.c
@@ -2,31 +2,34 @@
 #include <string.h>
 #include <locale.h>
 
-#define TAM 51
-#define CANT 10
+#define COLS 51
+#define ROWS 3
 
 main(){
-	char names[CANT][TAM];
+	char names[ROWS][COLS];
 	int i = 0, j = 0;
 	
 	setlocale(LC_ALL, "Spanish");
 	
-	for(i = 0; i < CANT; i++){
+	for(i = 0; i < ROWS; i++){
 		strcpy(names[i], "");
 	}
 	
 	puts("Introduce nombre y apellido");
-	for(i = 0; i < CANT; i++){
+	for(i = 0; i < ROWS; i++){
 		gets(names[i]);
 	}
 	
-	puts("Introduce el índice de un nombre: ");
+	puts("Introduce el índice de un nombre (<=0 para terminar): ");
 	scanf("%d", &i);
 	fflush(stdin);
 	while(i > 0){
-		puts(names[i-1]);
+		if (i - 1 < ROWS)
+			puts(names[i-1]);
+		else
+			puts("Posicion fuera de rango");
 		
-		puts("Introduce el índice de un nombre: ");
+		puts("Introduce el índice de un nombre (0 para terminar): ");
 		scanf("%d", &i);
 		fflush(stdin);
 	}
