3ef69f2b4a8128444903fe9dd41b28d4d36d30b5
NexCreep <codenexcreep@gmail.com>
2022-01-19 22:51:59 +0100

    Update 22:51 19/01

--- Entregas/lopez-bravo_raul_09.c
+++ Entregas/lopez-bravo_raul_09.c
@@ -1,22 +1,98 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <locale.h>
 
-int strlenC(int intarray[]);
+#define TAM 10
+
+void moreDigit(int array[], int *result);
+void locateDigInt(int array[], int digit, int *result);
 
 main(){
-	int arrayInt[]= {0, 4, 4, 6, 8, 7, 9, 0};
+	int arrayInt[TAM] = {0, 0, 9, 4, 1, 2, 2, 2, 7, 0};
+	int r = 0;
+	char rstr[5]="";
+	int digit = 0;
 	
-	printf("Es %d", strlenC(arrayInt));
+	setlocale(LC_CTYPE, "Spanish");
+	
+	moreDigit(arrayInt, &r);
+	printf("El digito que se repite más es en el array es %d\n", r);
+	
+	printf("Introduce un digito a buscar:\n");
+	scanf("%d", &digit);
+	fflush(stdin);
+	
+	r=0;
+	locateDigInt(arrayInt, digit, &r);
+	if(r)
+		printf("%d se encuentra en el array", digit);
+	else
+		printf("%d no se encuentra en el array", digit);
+		
+	locateDigStr(arrayInt, digit, &r);
+	if(strcmp(r, "uno"))
+		printf("%d se encuentra en el array", digit);
+	else
+		printf("%d no se encuentra en el array", digit);
 	
 	return 0;
 }
 
-int strlenC(int arrayInt[]){
-	int len = 0;
+void moreDigit(int array[], int *result){
+	int more = 0, morecont = 0, cont = 0;
 	
-	printf("%d\n", sizeof(arrayInt) / sizeof(arrayInt[0]));
+	int i = 0, j = 0;
 	
-	len = sizeof(arrayInt) / sizeof(arrayInt[0]);
+	more = array[0];
+	for(i = 0; i < TAM; i++){
+		for(j = 0; j < TAM; j++){
+			if(array[j] == array[i])
+				cont++;
+		}
+		if(morecont < cont){
+			morecont = cont;
+			more = array[i];
+		}
+		cont = 0;
+	}
 	
-	return len;
+	*result = more;
 }
+
+void locateDigInt(int array[], int digit, int *result){
+	int i = 0, flag = 0;
+	
+	i=0;
+	flag = 0;
+	while(i < TAM && !flag){
+		if(array[i] == digit)
+			flag = 1;
+		i++;
+	}	
+
+	*result = flag;
+}
+
+void locateDigStr(int array[], int digit, char *result){
+	int i = 0;
+	char flag[5]="";
+	
+	i=0;
+	flag = "cero";
+	while(i < TAM && strcmp(flag, "cero")){
+		if(array[i] == digit)
+			strcpy(flag, "uno")
+		i++;
+	}	
+
+	*result = flag;
+}
+
+////////////////////////////////
+/*
+int strlen(const char *str){
+	const char *s;
+	for(s = str; *s; s++);
+	
+	return (s - str);
+}
+*/
